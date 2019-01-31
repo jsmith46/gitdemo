cce0cbe7d809122bcc5411c23d569b56bff3e91e
jsmith46 <40039701+jsmith46@users.noreply.github.com>
2019-01-30 17:35:14 -0700

    Add files via upload

---
+++ GithubTest/app/src/main/java/com/example/githubtest/Shape.java
@@ -0,0 +1,31 @@
+package com.example.githubtest;
+/**
+ * Created by Team 15 on 01/30/2019
+ */
+
+public abstract class Shape {
+
+    private int  x;
+
+    private int y;
+
+    public void Shape(){
+
+        x = 0;
+        y = 0;
+
+    }
+
+    public void setCoords(int a, int b){
+
+        x = a;
+        y = b;
+    }
+
+    public int[] getCoords(){
+        int c[] = new int[2];
+        c[0] = x;
+        c[1] = y;
+        return c;
+    }
+}
